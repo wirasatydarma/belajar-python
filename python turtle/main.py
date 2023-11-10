792683fdad4abafeaafd5b6579335eba7dd2204b
wira <wira.satya@student.alhazen.academy>
2023-11-10 09:26:00 +0700

    file dari replit

---
+++ python turtle/main.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+getscreen()
+
+bgcolor("pink")
+pencolor("blue")
+pensize(2)
+shape("turtle")
+speed(0)
+fillcolor("white")
+begin_fill()
+
+init_size = 30
+for i in range(36):
+  for i in range(4):
+    forward(init_size)
+    left(360/4)
+    left(10) 
+   
+
+done()
+
