916bb74989aabc038beb0651d881a8dd9c31cd70
wira satya darma <wira.satya@student.alhazen.academy>
2023-11-10 17:13:46 +0700

    string color and emoji

---
+++ PIP Package Manager/main.py
@@ -0,0 +1,6 @@
+from stringcolor import *
+from emoji import *
+print(cs("wira satya darma", "pink"))
+print(bold("selamat datang"))
+print(cs("wira", "gold") .underline())
+print(cs("Nama Saya", "blue", "white"))
